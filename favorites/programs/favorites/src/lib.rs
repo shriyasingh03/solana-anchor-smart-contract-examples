d53bc9e92d3986033d6d462702a833963b431ef9
sony <sony@DESKTOP-DQ3DJIF.>
2026-06-26 08:48:30 +0000

    Create a program that stores a counter in a PDA (Program Derived Address) and increments it each time someone calls the instruction

---
+++ favorites/programs/favorites/src/lib.rs
@@ -0,0 +1,56 @@
+use anchor_lang::prelude::*;
+
+declare_id!("FxVj5yvW1M3jKM2CAoWLHBBvbaoKhEtzj9Un2Twu3Juw");
+
+pub const ANCHOR_DISCRIMINATOR_SIZE: usize = 8;
+
+#[program]
+pub mod favorites {
+    use super::*;
+
+    pub fn set_favorites(
+        context: Context<SetFavorites>,
+        number: u64,
+        color: String,
+        hobbies: Vec<String>,
+    ) -> Result<()> {
+        msg!("Greetings from {}", context.program_id);
+        let user_public_key = context.accounts.user.key();
+        msg!(
+            "User {user_public_key}'s favorite number is {number}, favorite color is: {color}, and their hobbies are {hobbies:?}",
+        );
+
+        // Manual assignment instead of set_inner()
+        let favorites = &mut context.accounts.favorites;
+        favorites.number = number;
+        favorites.color = color;
+        favorites.hobbies = hobbies;
+
+        Ok(())
+    }
+}
+
+#[account]
+#[derive(InitSpace)]
+pub struct Favorites {
+    pub number: u64,
+    #[max_len(50)]
+    pub color: String,
+    #[max_len(5, 50)]
+    pub hobbies: Vec<String>,
+}
+
+#[derive(Accounts)]
+pub struct SetFavorites<'info> {
+    #[account(mut)]
+    pub user: Signer<'info>,
+    #[account(
+        init_if_needed,
+        payer = user,
+        space = ANCHOR_DISCRIMINATOR_SIZE + Favorites::INIT_SPACE,
+        seeds = [b"favorites", user.key().as_ref()],
+        bump
+    )]
+    pub favorites: Account<'info, Favorites>,
+    pub system_program: Program<'info, System>,
+}
